65312dd138857c38f49d3ca12f6a81dd56a4e69d
trungtin252 <laitrungtin21@gmail.com>
2025-02-20 19:36:08 +0700

    Hien thi danh sach cac contact lay ve tu server

---
+++ Frontend/contactbook-frontend/src/main.js
@@ -0,0 +1,8 @@
+import "./assets/main.css";
+import { createApp } from "vue";
+import App from "./App.vue";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "@fortawesome/fontawesome-free/css/all.css";
+import router from "./router";
+
+createApp(App).use(router).mount("#app");
